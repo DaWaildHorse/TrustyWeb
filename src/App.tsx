2fed94dd91177886951031040c60577b0a2ddb29
UltiimateDog <73492621+UltiimateDog@users.noreply.github.com>
2024-06-18 22:01:08 -0600

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,35 @@
 import React, { useState } from 'react';
-//import axios from 'axios'; // Import axios for API requests
 import protegerIcon from './assets/proteger.png';
 import './App.css';
 import ReviewTextView from './ReviewTextView';
-import Chat from './Chat.tsx'; // Import the Chat component
+
+//Hola
 
 const App: React.FC = () => {
   const [icon] = useState(protegerIcon);
   const [currentView, setCurrentView] = useState('home');
 
+
   const renderView = () => {
-    switch(currentView) {
-      case 'home':
-        return (
-          <>
-            <h1>TrustyWeb</h1>
-            <div className="card">
-              <div className="shield">
-                <div className="shield-icon">
-                  <img src={icon} alt="Icon" />
-                </div>
-                <h2>Fuente Confiable</h2>
+    if (currentView === 'home') {
+      return (
+        <>
+          <h1>TrustyWeb</h1>
+          <div className="card">
+            <div className="shield">
+              <div className="shield-icon">
+                <img src={icon} alt="Icon" />
               </div>
-              <button className="review-button" onClick={() => setCurrentView('review')}>
-                Revisa el texto
-              </button>
-              <button className="chat-button" onClick={() => setCurrentView('chat')}>
-                Chat with AI
-              </button>
+              <h2>Fuente Confiable</h2>
             </div>
-          </>
-        );
-      case 'review':
-        return <ReviewTextView navigateToHome={() => setCurrentView('home')} />;
-      case 'chat':
-        return <Chat />;
-      default:
-        return <div>View not found</div>;
+            <button className="review-button" onClick={() => setCurrentView('review')}>
+              Revisa el texto
+            </button>
+          </div>
+        </>
+      );
+    } else if (currentView === 'review') {
+      return <ReviewTextView navigateToHome={() => setCurrentView('home')} />;
     }
   };
 
