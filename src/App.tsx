04955f08fb3dca3de4e69c19989b4c1a91560084
Rodrigo Espíritu Berra <107639915+RodEsB@users.noreply.github.com>
2024-06-18 18:31:53 -0600

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import protegerIcon from './assets/proteger.png';
+import './App.css';
+import ReviewTextView from './ReviewTextView';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+  const [icon] = useState(protegerIcon);
+  const [currentView, setCurrentView] = useState('home');
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <p>Alan peruano</p>
-    </>
-  )
-}
 
-export default App
+  const renderView = () => {
+    if (currentView === 'home') {
+      return (
+        <>
+          <h1>TrustyWeb</h1>
+          <div className="card">
+            <div className="shield">
+              <div className="shield-icon">
+                <img src={icon} alt="Icon" />
+              </div>
+              <h2>Fuente Confiable</h2>
+            </div>
+            <button className="review-button" onClick={() => setCurrentView('review')}>
+              Revisa el texto
+            </button>
+          </div>
+        </>
+      );
+    } else if (currentView === 'review') {
+      return <ReviewTextView navigateToHome={() => setCurrentView('home')} />;
+    }
+  };
+
+  return <div className="App">{renderView()}</div>;
+};
+
+export default App;
